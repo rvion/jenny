51039159e77006a31d084e24886a13e4c642a676
Rémi Vion <vion.remi@gmail.com>
2016-12-27 14:15:34 +0100

    initial test suite

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,54 @@
 module Test.Main where
 
 import Prelude
+import Node.FS.Aff as FS
+import Node.FS.Sync as Sync
+import Test.Unit.Assert as Assert
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Class (liftEff)
+import Data.Array (filterM)
+import Data.Foldable (for_)
+import Data.String (joinWith)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readdir)
+import Node.FS.Stats (isDirectory)
+import Run (applyTemplate, buildTargets)
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: Eff _ Unit
 main = do
-  log "You should add some tests."
+    -- IDEA take a list of specific test test to run
+    -- TODO add suite for inline change
+
+    -- find all tests folders
+    rootFiles <- Sync.readdir "test"
+    folders <- filterM
+        (\f -> isDirectory <$> Sync.stat ("test/" <> f))
+        rootFiles
+
+    runTest do
+        suite "generated code match" $
+            for_ folders \folder -> test folder do
+
+                  -- apply template
+                  liftEff $ do
+                    applyTemplate false (jennyPath folder)
+                    pure true
+
+                  when true do
+                    -- FIXME check recursively
+                    files <- readdir (correctPath folder)
+                    for_ files \file -> do
+                        out <- FS.readTextFile UTF8 (outPath folder <> file)
+                        correct <- FS.readTextFile UTF8 ((correctPath folder) <> file)
+                        Assert.equal out correct
+
+jennyPath :: String -> String
+jennyPath folder = "test/" <> folder <> "/test.jenny"
+
+correctPath :: String -> String
+correctPath folder = "test/"<> folder <> "/correct/"
+
+outPath :: String -> String
+outPath folder = "test/"<> folder <> "/out/"
