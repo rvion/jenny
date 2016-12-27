cba9d42529ecfe30f6290eadcdf1a65c4c21f818
Rémi Vion <vion.remi@gmail.com>
2016-12-27 18:50:55 +0100

    add watch option

--- src/Option.purs
+++ src/Option.purs
@@ -12,14 +12,16 @@ import Node.Yargs.Setup (YargsSetup, example, usage)
 type Options = {
   templates :: Array String,
   prefixPath :: String,
-  debug :: Boolean
+  debug :: Boolean,
+  watch :: Boolean
 }
 
 getOpts :: forall eff. Eff ( err :: EXCEPTION , console :: CONSOLE | eff ) Options
 getOpts = runY setup parser
   where
     parser :: Y (Eff ( err :: EXCEPTION , console :: CONSOLE | eff ) Options)
-    parser = (\templates prefixPath debug -> pure {templates, prefixPath, debug})
+    parser = (\templates  prefixPath  debug  watch ->
+         pure {templates, prefixPath, debug, watch})
       <$> yarg "t" ["template"]
         (Just "template file")
         (Right "At least one template is required")
@@ -28,7 +30,9 @@ getOpts = runY setup parser
         (Just "local output path prefix")
         (Right "")
         false
-      <*> flag "d" []
+      <*> flag "d" ["debug"]
+        (Just "debug")
+      <*> flag "w" ["watch", "live"]
         (Just "debug")
 
     setup :: YargsSetup
